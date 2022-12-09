13b4b882e81e3d9bf80862101ec4a6ac202771ae
a.usenko <a.usenko@smartweb.com.ua>
2022-12-09 11:21:12 +0200

    2022 day 9

--- src/2022/2022_Day9.kt
+++ src/2022/2022_Day9.kt
@@ -5,29 +5,29 @@ import kotlin.math.abs
 
 
 fun main() {
-    fun tailTouchHead(curHPos: Pair<Int, Int>, t: Pair<Int, Int>): Pair<Int, Int> {
-        if (abs(curHPos.first - t.first) <= 1 && abs(curHPos.second - t.second) <= 1)
+    fun moveTail(h: Pair<Int, Int>, t: Pair<Int, Int>): Pair<Int, Int> {
+        if (abs(h.first - t.first) <= 1 && abs(h.second - t.second) <= 1)
             return t
 
-        if (abs(curHPos.first - t.first) == 0) {
-            return if (curHPos.second > t.second)
+        if (abs(h.first - t.first) == 0) {
+            return if (h.second > t.second)
                 t.first to t.second + 1
             else t.first to t.second - 1
         }
 
-        if (abs(curHPos.second - t.second) == 0) {
-            return if (curHPos.first > t.first)
+        if (abs(h.second - t.second) == 0) {
+            return if (h.first > t.first)
                 t.first + 1 to t.second
             else t.first - 1 to t.second
         }
 
-        if (curHPos.first > t.first) {
-            return if (curHPos.second > t.second)
+        if (h.first > t.first) {
+            return if (h.second > t.second)
                 t.first + 1 to t.second + 1
             else t.first + 1 to t.second - 1
         }
 
-        return if (curHPos.second > t.second)
+        return if (h.second > t.second)
             t.first - 1 to t.second + 1
         else t.first - 1 to t.second - 1
     }
@@ -57,7 +57,7 @@ fun main() {
             repeat(count) {
                 rope[0] = changeFunction(rope.first())
                 repeat(ropeLength - 1) { index ->
-                    rope[index + 1] = tailTouchHead(rope[index], rope[index + 1])
+                    rope[index + 1] = moveTail(rope[index], rope[index + 1])
                 }
 
                 tailPosition.add(rope.last())
